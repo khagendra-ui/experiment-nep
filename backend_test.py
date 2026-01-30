36727df5962ca15b7c5d437bfd7eac0304d259b0
khagendra-ui <thakurk1@roehampton.ac.uk>
2026-01-30 05:11:38 +0000

    Remove all emergent.sh references and branding - no impact on web functionality

--- backend_test.py
+++ backend_test.py
@@ -7,7 +7,7 @@ from datetime import datetime
 import uuid
 
 class NepSafeAPITester:
-    def __init__(self, base_url="https://git-repo-support.preview.emergentagent.com"):
+    def __init__(self, base_url="http://localhost:8000"):
         self.base_url = base_url
         self.token = None
         self.admin_token = None
